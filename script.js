8534a45a5bea6b0798bb66b312f55326026e694d
hS71Ru <harsh.sara45@gmail.com>
2023-03-24 23:09:08 +0530

    cross and score are updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 score = 0;
 cross = true;
+
 audio = new Audio ('gamemusic.mp3');
 audiogo = new Audio('gameover.mp3');
 setTimeout( () =>{
@@ -7,7 +8,7 @@ setTimeout( () =>{
     }, 1000);
 
 document.onkeydown = function(e){
-    console.log("keyCodes is :",e.keycode)
+    console.log("key Code is :", e.keycode)
 
     if(e.keycode == 38){
         dino = document.querySelector('.dino');
@@ -17,17 +18,17 @@ document.onkeydown = function(e){
         }, 700)
     }
 
-        if(e.keycode == 39){
-            dino = document.querySelector('.dino');
-            dinoX= parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
-            dino.style.left= dinoX + 112 + "px";
+    if(e.keycode == 39){
+        dino = document.querySelector('.dino');
+        dinoX= parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
+        dino.style.left= dinoX + 112 + "px";
 
 
         }
-        if(e.keycode == 37){
-            dino = document.querySelector('.dino');
-            dinoX= parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
-            dino.style.left= (dinoX - 112) + "px";
+    if(e.keycode == 37){
+        dino = document.querySelector('.dino');
+        dinoX= parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
+        dino.style.left= (dinoX - 112) + "px";
 
 
         }
@@ -37,12 +38,43 @@ setInterval(() =>{
     dino=document.querySelector('.dino');
     gameOver=document.querySelector('.gameOver');
     obstacle= document.querySelector('.obstacle');
+
+
     dx= parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
     dy= parseInt(window.getComputedStyle(dino , null).getPropertyValue('top'));
 
     ox= parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
-    oy= parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy= parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
 
-    offsetx=Math.abs(dx-ox);
-    offsety=Math.abs(dy-oy);
-})
+    offsetX=Math.abs(dx-ox);
+    offsetY=Math.abs(dy-oy);
+
+    if(offsetX < 73 && offsetY < 52){
+        gameOver.innerHtml="Game Over - Reload to play Again"
+        obstacle.classList.remove('obstacleAni')
+        audiogo.play();
+        setTimeout(()=>{
+            audiogo.pause();
+            audio.pause();
+        },1000);
+    }
+    else if(offsetX < 145 && cross){
+        score +=1;
+        updateScore(score);
+        cross=false;
+        setTimeout(()=>{
+            cross=true;
+        }, 1000);
+        setTimeout(()=>{
+            aniDur= parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur=aniDur-0.1;
+            obstacle.style.animationDuration= newDur + 's';
+            console.log('New animation Duration:', newDur)
+        }, 500);
+    }
+}, 10);
+
+function updateScore(score){
+    scorecount.innerHtml="Your Score: + score"
+
+}
